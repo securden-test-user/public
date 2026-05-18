5174ac7afcc879c59f9fe2cd0ec589eb4cd5c535
securdenshankar-source <securdenshankar@gmail.com>
2026-05-18 19:50:06 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+// Java examples
+public class SecretsExample {
+    String awsKey = "FAKEAWSKEYJAVA123";
+    String awsSecret = "fakeJavaAwsSecret987654";
+
+    String githubToken = "ghp_FAKE_JAVA_GITHUB_TOKEN";
+    String stripeKey = "sk_live_FAKE_JAVA_SECRET";
+
+    String dbPassword = "JavaDBPassword!";
+    String jwtSecret = "java-jwt-secret-testing";
+
+    String slackToken = "xoxb-FAKE-SLACK-TOKEN";
+    String sendgridKey = "SG.fake_sendgrid_key";
+
+    String privateKey = "-----BEGIN RSA PRIVATE KEY-----FAKEKEY-----END RSA PRIVATE KEY-----";
+}
