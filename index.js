6be15b23a50a6d3fb0f46da98cd753bf0f952687
securdenshankar-source <securdenshankar@gmail.com>
2026-05-18 19:50:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// JavaScript examples
+const apiKey = "FAKE_JS_API_KEY";
+const githubToken = "ghp_FAKE_JS_GITHUB";
+
+const firebaseConfig = {
+  apiKey: "FAKE_FIREBASE_API_KEY",
+  authDomain: "fake-project.firebaseapp.com"
+};
+
+const jwtSecret = "javascript-secret-key";
+const stripeSecret = "sk_test_FAKE_JS_SECRET";
+
+const dbPassword = "NodePassword123!";
+const webhook = "https://discord.com/api/webhooks/FAKE/WEBHOOK";
