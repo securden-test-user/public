5f0f0ad8a3bd6cafb014f826685e36559678f62e
securdenshankar-source <securdenshankar@gmail.com>
2026-05-18 19:49:07 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,17 @@
+# Python examples
+AWS_ACCESS_KEY_ID = "FAKEAWSACCESSKEY12345"
+AWS_SECRET_ACCESS_KEY = "fakeAwsSecretKey_NotReal_1234567890"
+
+github_token = "ghp_FAKEgithubTokenForScannerTestingOnly"
+slack_webhook = "https://hooks.slack.com/services/FAKE/WEBHOOK/TEST"
+
+stripe_secret = "sk_test_FAKE_STRIPE_SECRET_KEY"
+twilio_auth = "fake_twilio_auth_token_987654321"
+
+db_password = "SuperSecretPassword123!"
+jwt_secret = "jwt_fake_secret_for_testing"
+
+api_key = "FAKE_API_KEY_ABC123XYZ"
+private_key = """-----BEGIN PRIVATE KEY-----
+FAKE_PRIVATE_KEY_CONTENT
+-----END PRIVATE KEY-----"""
